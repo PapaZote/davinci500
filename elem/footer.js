bc4ce2d97ba1400c7bc18f6036fad557c09b5618
PapaZote <64790281+PapaZote@users.noreply.github.com>
2020-05-21 11:31:52 -0500

    Add files via upload
    Commit index.html as main page

---
+++ elem/footer.js
@@ -0,0 +1,17 @@
+document.write('\
+<div class="basefooter">\
+	<div class="redes">\
+		<a target="_blank" href="https://www.facebook.com/500-A%C3%B1os-de-Leonardo-da-Vinci-107354170987628/?modal=admin_todo_tour"><img src="imag/iconos/face.png" alt=""></a>\
+		<a target="_blank" href="https://twitter.com/leonardo500anos"><img src="imag/iconos/twitter.png" alt=""></a>\
+		<a target="_blank" href="https://www.youtube.com/channel/UCALQaJ3n4Ip_G6sL1eAb5GQ"><img src="imag/iconos/youtube.png" alt=""></a>\
+		<a target="_blank" href="https://www.tumblr.com/dashboard/blog/leodavinci500"><img src="imag/iconos/tumblr.png" alt=""></a>\
+		<a target="_blank" href="https://www.instagram.com/davinciquinientos/"><img src="imag/iconos/insta.png" alt=""></a>\
+		<div class="copyright">Copyright 500 Años da Vinci | todos los derechos reservados. <a href="#top">Volver Arriba</a></div>\
+	</div>\
+\
+	<div class="logo-div">\
+		<img src="imag/logo.png" alt="">\
+	</div>\
+</div>\
+\
+');
---
+++ elem/header.js
@@ -0,0 +1,68 @@
+document.write('\
+\
+	<link rel="stylesheet" href="../styles/style.css">\
+	<div class="baseheader header2">\
+		<div class="divlogo">\
+			<a href="index.html"><img src="imag/logo.png"></a>\
+		</div>\
+		<div class="opcionesmenu">\
+\
+			<!--Aquí comienzan las actividades -->\
+\
+			<a href="actividades.html">\
+				<div>Actividades</div>\
+				<div class="barmenu"></div>\
+			</a>\
+\
+			<a href="pabellon.html">\
+				<div>Pabellón</div>\
+				<div class="barmenu"></div>\
+			</a>\
+\
+			<a href="galeria.html">\
+				<div>Galería</div>\
+				<div class="barmenu"></div>\
+			</a>\
+\
+			<a href="inscribirse.html">\
+				<div>Inscribirse</div>\
+				<div class="barmenu"></div>\
+			</a>\
+\
+			<!--Aquí terminan las actividades -->\
+\
+		</div>\
+	</div>\
+\
+\
+	<div class="baseheader-responsive">\
+		<div class="divlogo2">\
+			<a href="index.html"><img src="imag/logo.png"></a>\
+		</div>\
+		<div class="menubars"><img class="btnmenu" src="imag/iconmenu.png"></div>\
+		<div class="opcionesmenu2">\
+\
+			<!--Aquí comienzan las actividades -->\
+\
+			<a href="actividades.html">\
+				<div>Actividades</div>\
+			</a>\
+\
+			<a href="pabellon.html">\
+				<div>Pabellón</div>\
+			</a>\
+\
+			<a href="galeria.html">\
+				<div>Galería</div>\
+			</a>\
+\
+			<a href="inscribirse.html">\
+				<div>Inscribirse</div>\
+			</a>\
+\
+			<!--Aquí terminan las actividades -->\
+\
+		</div>\
+	</div>\
+\
+');
